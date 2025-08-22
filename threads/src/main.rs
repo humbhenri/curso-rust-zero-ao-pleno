e36548083f651fdd974b3ab623a1aa3c124af09b
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2025-08-22 13:40:14 -0300

    tarefa threas soma vetor

---
+++ threads/src/main.rs
@@ -0,0 +1,23 @@
+use std::thread;
+
+fn main() {
+    let v = vec![10; 10];
+    let len = v.len();
+    let nthreads = 4;
+    let chunk_size = len / nthreads;
+    let mut threads = Vec::new();
+    for i in 0..nthreads {
+        let vstart = i * chunk_size;
+        let vend = if i == nthreads - 1 { len } else { (i+1) * chunk_size };
+        let slice = v[vstart..vend].to_vec();
+        threads.push(thread::spawn(move || {
+            let sum: i32 = slice.iter().sum();
+            sum
+        }));
+    }
+    let mut sum = 0;
+    for t in threads {
+        sum += t.join().unwrap();
+    }
+    println!("{}", sum);
+}
