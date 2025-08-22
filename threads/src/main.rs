e3082936a6788f1c390236c5fea282e42777bdae
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2025-08-22 13:44:46 -0300

    torna mais eficiente ao evitar copiar o vector

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,16 +1,24 @@
+use std::sync::Arc;
 use std::thread;
 
 fn main() {
     let v = vec![10; 10];
+    let v = Arc::new(v);
     let len = v.len();
     let nthreads = 4;
     let chunk_size = len / nthreads;
     let mut threads = Vec::new();
     for i in 0..nthreads {
         let vstart = i * chunk_size;
-        let vend = if i == nthreads - 1 { len } else { (i+1) * chunk_size };
-        let slice = v[vstart..vend].to_vec();
+        let vend = if i == nthreads - 1 {
+            len
+        } else {
+            (i + 1) * chunk_size
+        };
+        //let slice = v[vstart..vend].to_vec();
+        let vref = Arc::clone(&v);
         threads.push(thread::spawn(move || {
+            let slice = &vref[vstart..vend];
             let sum: i32 = slice.iter().sum();
             sum
         }));
